58ed9bba009c5892fb7224d21161c6b338fed155
deepanshuj12 <ddeepanshuj29@gmail.com>
2026-01-29 14:08:58 +0530

    issue fixed for deployment

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors"; 
 import db from "./routes/db.js";
 import pastes from "./routes/pastes.js";
 
 const app = express();
 
+app.use(cors({
+  origin: process.env.PUBLIC_BASE_URL // e.g. https://pastebin-frontend-henna.vercel.app
+}));
+
 app.use(express.json());
 
 app.use("/api/db", db);
 app.use("/api/pastes", pastes);
-
 
 export default app;
